0f2dafcf210e8a0b8d5b44934dbd6d2bc3d8961e
daank12 <96422551+daank12@users.noreply.github.com>
2023-07-03 14:52:27 -0300

    Primer brunch

---
+++ 02--tic-tac-toe/src/App.jsx
@@ -0,0 +1,39 @@
+import { Children } from 'react';
+import './App.css'
+const TURNS ={
+    X: 'x',
+    O: 'o'
+}
+const board = Array(9).fill(null);
+const Square = ({children, updateBoard, index }) => {
+    return (
+        <div className='square'>
+            {children}
+        </div>
+    )
+}
+
+function App() {
+return (
+    <main className='board'>
+        <h1>tic tac toe</h1>
+        <section className="game">
+            {
+              board.map(( a ,index) => {
+                return (
+                    <Square
+                     key={index}
+                     index={index}
+                     >
+                        {index}
+                     </Square>
+            )
+        })
+    }
+</section>
+
+</main>
+)
+}
+
+export default App
